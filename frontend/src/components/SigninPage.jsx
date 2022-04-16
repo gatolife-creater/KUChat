7678acec61a2c2ecbaec096fa2b662393a5f3aca
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-04-16 23:13:23 +0900

    サインイン、サインアップ画面のチェックボックスを削除、オートコンプリートを無効化。

--- frontend/src/components/SigninPage.jsx
+++ frontend/src/components/SigninPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SignupPage = () => {
     return(
             <main className="form-signin text-center">
-                <form method="post" action="/signin-attempt">
+                <form method="post" action="/signin-attempt" autocomplete="off">
                     <img className="mb-4" src="https://pbs.twimg.com/profile_images/1366595750/3_400x400.jpg" alt="" width="72" height="72"/>
                     <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
                 
@@ -16,12 +16,7 @@ const SignupPage = () => {
                         <label htmlFor="floatingPassword">Password</label>
                     </div>
                 
-                    <div className="checkbox mb-3">
-                        <label>
-                            <input type="checkbox" value="remember-me"/> Remember me
-                        </label>
-                    </div>
-                    <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                    <button className="mt-5 w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
                     <p className="mt-5 mb-3">No address with you? Please <a href="/signup">sign up</a>.</p>
                     <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
                 </form>
--- frontend/src/components/SignupPage.jsx
+++ frontend/src/components/SignupPage.jsx
@@ -11,21 +11,16 @@ const SignupPage = () => {
     return(
         <>
             <main className="form-signin text-center">
-                <form>
+                <form action="/generate-address">
                     <img className="mb-4" src="https://pbs.twimg.com/profile_images/1366595750/3_400x400.jpg" alt="" width="72" height="72"/>
                     <h1 className="h3 mb-3 fw-normal">Please sign up</h1>
                 
-                    <div className="form-floating">
+                    {/* <div className="form-floating">
                         <input type="password" className="form-control" id="floatingPassword" placeholder="Password" style={{borderTopRightRadius: "0.25rem", borderTopLeftRadius: "0.25rem"}}/>
                         <label htmlFor="floatingPassword">Password</label>
-                    </div>
+                    </div> */}
                 
-                    <div className="checkbox mb-3">
-                        <label>
-                            <input type="checkbox" value="remember-me"/> Remember me
-                        </label>
-                    </div>
-                    <button className="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+                    <button className="mt-5 w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
                     <p className="mt-5 mb-3">Already have address? Please <a href="/signin">sign in</a>.</p>
                     <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
                 </form>
