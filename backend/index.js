5bc2cae0789cfc026baae9a1f5809672270fa5a8
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-04-25 21:41:39 +0900

    bip39とcryptoを導入。

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,6 @@ const EC = require("elliptic").ec;
 //ビットコインのウォレットにも実際に使われるアルゴリズムらしい
 const ec = new EC("secp256k1");
 
-const key = ec.genKeyPair();
-const myKey = ec.keyFromPrivate("a1824ada2af9f5f2f9884e7e3444491fae672d0965e36f04c179ed3398e6883e");
-const walletAddress = key.getPublic("hex");
-const myWalletAddress = myKey.getPublic("hex");　
-console.log(key);
 // hexはおそらく16進数のこと。実際にmyWalletAddressは16進数になっている。
 
 // ブロックチェーンを生成
