6f245d3b0362c5cb89eb37b5d29dcefb2499faa7
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-05-10 06:30:00 +0900

    peer to peerテスト用コンポーネントを追加。

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import "./css/Error.scss";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { useState, useEffect, useReducer, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import HomePage from "./components/pages/HomePage";
 import WalletPage from "./components/pages/WalletPage";
@@ -18,37 +18,9 @@ import SearchPage from "./components/pages/SearchPage";
 import SignupPage from "./components/pages/SignupPage";
 import SigninPage from "./components/pages/SigninPage";
 import ErrorPage from "./components/pages/ErrorPage";
-
-import Gun from 'gun';
-
-const gun = Gun({
-  peers: [
-    'http://localhost:3030/gun'
-  ]
-})
-
-// create the initial state to hold the messages
-const initialState = {
-  messages: []
-}
-
-// Create a reducer that will update the messages array
-function reducer(state, message) {
-  return {
-    messages: [message, ...state.messages]
-  }
-}
+import PeerToPeer from "./components/pages/p2ptest";
 
 const App = () => {
-
-  // the form state manages the form input for creating a new message
-  const [formState, setForm] = useState({
-    name: '', message: ''
-  })
-
-   // initialize the reducer & state for holding the messages array
-   const [state, dispatch] = useReducer(reducer, initialState)
-
   const [blockchain, setMessage] = useState([]);
 
   const isFirstRender = useRef(false);
@@ -62,35 +34,8 @@ const App = () => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setMessage(data.blockchain));
-      const messages = gun.get('messages')
-      messages.map().on(m => {
-        dispatch({
-          name: m.name,
-          message: m.message,
-          createdAt: m.createdAt
-        })
-      })
   }, []);
 
-
-  // set a new message in gun, update the local state to reset the form field
-  function saveMessage() {
-    const messages = gun.get('messages')
-    messages.set({
-      name: formState.name,
-      message: formState.message,
-      createdAt: Date.now()
-    })
-    setForm({
-      name: '', message: ''
-    })
-  }
-
-  // update the form state as the user types
-  function onChange(e) {
-    setForm({ ...formState, [e.target.name]: e.target.value  })
-  }
-  
   return (
     <Router>
       <Routes>
@@ -118,6 +63,7 @@ const App = () => {
         />
         <Route path="/signup" element={<SignupPage />} exact />
         <Route path="/signin" element={<SigninPage />} exact />
+        <Route path="/p2p" element={<PeerToPeer />} exact />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
---
+++ frontend/src/components/pages/p2ptest.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from "../parts/NavBar";
+
+const PeerToPeer = () => {
+    return(
+        <>
+            <NavBar />
+            <main>
+                this is a test.
+            </main>
+        </>
+    )
+}
+
+export default PeerToPeer;
