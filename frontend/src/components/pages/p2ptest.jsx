73d082ad56c3ede6cf5c962ecd5c3bf5807195ad
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-05-10 20:00:59 +0900

    peer to peer のテストを実装。

--- frontend/src/components/pages/p2ptest.jsx
+++ frontend/src/components/pages/p2ptest.jsx
@@ -1,15 +1,96 @@
-import React from 'react';
+import { useEffect, useState, useReducer } from 'react'
+import Gun from 'gun'
 import NavBar from "../parts/NavBar";
 
+
+// initialize gun locally
+const gun = Gun({
+  peers: [
+    'http://localhost:3007/gun'
+  ]
+})
+
+// create the initial state to hold the messages
+const initialState = {
+  messages: []
+}
+
+// Create a reducer that will update the messages array
+function reducer(state, message) {
+  return {
+    messages: [message, ...state.messages]
+  }
+}
+
 const PeerToPeer = () => {
-    return(
-        <>
+    /// the form state manages the form input for creating a new message
+    const [formState, setForm] = useState({
+        name: '', message: ''
+      })
+    
+      // initialize the reducer & state for holding the messages array
+      const [state, dispatch] = useReducer(reducer, initialState)
+    
+      // when the app loads, fetch the current messages and load them into the state
+      // this also subscribes to new data as it changes and updates the local state
+      useEffect(() => {
+        const messages = gun.get('messages')
+        messages.map().on(m => {
+          dispatch({
+            name: m.name,
+            message: m.message,
+            createdAt: m.createdAt
+          })
+        })
+      }, [])
+    
+      // set a new message in gun, update the local state to reset the form field
+      function saveMessage() {
+        const messages = gun.get('messages')
+        messages.set({
+          name: formState.name,
+          message: formState.message,
+          createdAt: Date.now()
+        })
+        setForm({
+          name: '', message: ''
+        })
+      }
+    
+      // update the form state as the user types
+      function onChange(e) {
+        setForm({ ...formState, [e.target.name]: e.target.value  })
+      }
+    
+      return (
+          <>
             <NavBar />
-            <main>
-                this is a test.
-            </main>
-        </>
-    )
+            <div style={{ padding: 30 }}>
+                <input
+                    onChange={onChange}
+                    placeholder="Name"
+                    name="name"
+                    value={formState.name}
+                />
+                <input
+                    onChange={onChange}
+                    placeholder="Message"
+                    name="message"
+                    value={formState.message}
+                />
+                <button onClick={saveMessage}>Send Message</button>
+                {
+                    state.messages.map(message => (
+                    <div key={message.createdAt}>
+                        <h2>{message.message}</h2>
+                        <h3>From: {message.name}</h3>
+                        <p>Date: {message.createdAt}</p>
+                    </div>
+                    ))
+                }
+            </div>
+          </>
+      )
 }
 
 export default PeerToPeer;
