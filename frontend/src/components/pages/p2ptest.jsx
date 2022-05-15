d4f1ff0fc24382326b4375f6c312062177e8345e
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-05-15 15:42:07 +0900

    クライアントサイドのブロックチェーンで、マイニングやトランザクションのテストをした。

--- frontend/src/components/pages/p2ptest.jsx
+++ frontend/src/components/pages/p2ptest.jsx
@@ -1,28 +1,102 @@
+import {useState, useEffect} from "react";
 import Gun from "gun";
 import NavBar from "../parts/NavBar";
-import "./../../blockchain/index";
+import { Link } from "react-router-dom";
+
+import {Blockchain} from "./../../blockchain/blockchain";
+import {Transaction} from "./../../blockchain/transaction";
+
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+
+const kuchatBlockchain = new Blockchain();
+
 
 const gun = Gun({
   peers: ["http://localhost:3007/gun"],
 });
 
 const PeerToPeer = (props) => {
-  const { blockchain } = props;
   gun.get("blockchain").put({
-    blockchain: blockchain,
+    blockchain: kuchatBlockchain,
   });
   gun.get("blockchain").on((data, key) => {
     console.log("realtime updates:", JSON.parse(data.blockchain));
   });
 
   setInterval(() => {
-    gun.get("blockchain").put({ blockchain: JSON.stringify(blockchain) });
+    gun.get("blockchain").put({ blockchain: JSON.stringify(kuchatBlockchain) });
   }, 10000);
+
+  const createTransaction = () =>{
+    let privateKey = "go-go-go-this-is-test-key";
+    let fromAddress = ec.keyFromPrivate(privateKey).getPublic("hex");
+    let toAddress = "matthew.eth";
+    
+    kuchatBlockchain.minePendingTransactions(fromAddress);
+
+    let transaction = new Transaction(fromAddress, toAddress, 100, "僕は先日");
+
+    transaction.signTransaction(ec.keyFromPrivate(privateKey));
+    // 取引を保留する
+    kuchatBlockchain.addTransaction(transaction);
+    kuchatBlockchain.minePendingTransactions(fromAddress);
+
+    setChain(kuchatBlockchain.chain);
+  }
+
+  const [chain, setChain] = useState([]);
+
+  useEffect(() => {
+    setChain(kuchatBlockchain.chain);
+  },[])
 
   return (
     <>
       <NavBar />
-      <main>peer to peer messages</main>
+      <main>
+        <button className="btn btn-primary" onClick={createTransaction}>create transaction</button>
+        <div className="container">
+          <div className="row justifycontent-start">
+            {chain.map((block, index) => (
+              <div className="col-md-4 ms-auto">
+                <div className="card clickable-card">
+                  <Link
+                    to={`/block_details?number=${index}`}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <div className="card-header">
+                      Block : <span className="block_number">{index}</span>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item hash text-truncate">
+                        <span>Hash :</span>
+                        <br />
+                        {block.hash}
+                      </li>
+                      <li className="list-group-item previousHash text-truncate">
+                        <span>Previous Hash :</span>
+                        <br />
+                        {block.previousHash}
+                      </li>
+                      <li className="list-group-item nonce text-truncate">
+                        <span>Nonce :</span>
+                        <br />
+                        {block.nonce}
+                      </li>
+                      <li className="list-group-item timestamp text-truncate">
+                        <span>Timestamp :</span>
+                        <br />
+                        {block.timestamp}
+                      </li>
+                    </ul>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
     </>
   );
 };
